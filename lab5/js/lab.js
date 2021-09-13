e8ba441791772e4be688e34e0644b514a394daca
holyneotrin <tphanlow@csumb.edu>
2021-09-13 15:57:38 -0700

    lab 5 stuff pt 1

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+// lab.js - Play with variables
+// Author: Trinity Phan-Low <tphanlow@csumb.edu>
+// Created: September 13
+// License: Public Domain
+
+//  Declare variables to hold the make, model, color, and year of your vehicle, example, var year = 2004
+var make = "Ford";
+var model = "Taurus";
+var color = "Rusty";
+var year = "1995";
+
+// Declare a boolean variable ownIt for whether you own it or not.
+var ownIt = false;
+
+// Create a new variable age that subtracts the year from the current year
+age = 2021 - year;
+
+// Use document.writeln() to neatly output each of your variables
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
