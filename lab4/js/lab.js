493f30596345e4fa0076a6687076703dff031d64
holyneotrin <tphanlow@csumb.edu>
2021-09-12 23:34:13 -0700

    lab 4 pt 2

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,26 @@
-/
 // Summary.
 // Description.
 // @link URL
 // @file This file defines the MyClass class.
 // @author AuthorName.
 // @since x.x.x.
-/
+
+//First get your ingredients<br>Spam, white rice, soy sauce, sugar, large seaweed sheets, water, and furikake seasoning.
+// Cook the rice.
+//Cut up spam into 1cm slices, long ways.
+//Add splash of oil into pan. Fry spam on medium heat and drain the oil after.
+//While pan is still hot add soy sauce and sugar mixture 2:1 ratio in tablespoons. Keep on pan until carmelized. Place in a small dipping bowl.
+//Cut the seaweed sheets in half to get long rectangle shapes. Best achieved with scissors.
+//Place seaweed down, shiny side up. Then put rice in the middle, shaped into a rectangle shape.
+//Dash soysauce mixture on to the rice and then sprinkle furikake seasoning after. Add as much to your liking.
+//Place spam on top. Fold bottom side of the seaweed over the spam and rice.
+//With small amount of water on your fingers, moisten the folded seaweed surface. Then fold the other half of the seaweed. The wet side will hold the top half and keep the musubi from spil//ng out.
+//Enjoy!
+
+//Open the Photoshop program.
+//Click on the "Create New" button.
+//Pick a document size based on your project. You can use a default size listed in the left section or customize your own document on the right side of the window. The top bar has various document presets based off of the type of project.
+//To customize your document, pick the size. You have the option to measure the document in inches, centimeters, milimeters, picas, pixels, and points.
+//Choose your resolution of the document.
+//Pick your color mode and color mode.
+//Create your document by hitting "Create."
