3bf279c88efdae7f436ba78f84a89550b84a9cac
holyneotrin <tphanlow@csumb.edu>
2021-09-28 12:58:00 -0700

    lab 8

---
+++ lab8/js/lab8.js
@@ -0,0 +1,11 @@
+//Author: Trinity Phan-Low
+//Created 9.27.2021
+//Public Domain
+
+// function to add text to given element
+function main() {
+    // get element by id
+    outputElement = document.getElementById("js-output");
+    // change contents of element
+    outputElement.innerHTML = "Yo, 'sup?";
+}
