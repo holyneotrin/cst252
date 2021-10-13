f69d5e0078033d8345cddf7a0c6e8fc7628aac0c
holyneotrin <tphanlow@csumb.edu>
2021-10-13 12:15:04 -0700

    lab12

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -2,4 +2,21 @@
 // date created October 11, 2021
 // Public Domain
 
-var search = $("input:text").val()
+//adds green text to content
+$("#challenge-button").click(function(){
+  $("#content").removeClass("challenge2");
+  $("#content").toggleClass("challenge1");
+});
+
+//adds orange text to content
+$("#problem-button").click(function(){
+  $("#content").removeClass("problem2");
+  $("#content").toggleClass("problem1");
+});
+
+//adds purple text to content
+$("#result-button").click(function() {
+  $("#content").removeClass("result2");
+  $("#content").toggleClass("result1");
+
+});
