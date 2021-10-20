dbc261c1c525896879b5fd10ca0928aa82de1be9
holyneotrin <tphanlow@csumb.edu>
2021-10-20 12:29:55 -0700

    lab 14 update

--- lab14/js/lab14.js
+++ lab14/js/lab14.js
@@ -1,23 +1,32 @@
-// author: trinity phan-low
+ // author: trinity phan-low
 // date: 10.18.2021
 // public domain
 
-for (num=1; num <=200; num++) {
-		if (num % 15 == 0){
-    	console.log("FizzBuzz!")
-    }
-    //multiples of 3
-    if (num % 3 == 0) {
-        console.log("Fizz!");
-    }
+// click function from Matthew Lencioni
+$("#loops").click(function(){
+	$("#output").empty();
+		for (num=1; num <=200; num++) {
+			var outputStr = "";
+		// multiples of 15
+			if (num % 15 == 0){
+    		console.log("FizzBuzz!")
+    	};
+    // multiples of 3
+    	if (num % 3 == 0) {
+        outputStr += "Fizz!"
+    	};
     // multiples of 5
-    if (num % 5 == 0) {
-      console.log("Buzz!");
-    }
-    if (num % 7 == 0){
-      console.log("Boom!");
-    }
-    else {
-        console.log(num);
-    }
-    
+    	if (num % 5 == 0) {
+      	outputStr += "Buzz!"
+    	};
+    	if (num % 7 == 0){
+      	outputStr += "Boom!"
+			};
+	if (outputStr == ""){
+		$("#output").append(num + "<br>");
+	}
+	else {
+		$("#output").append(outputStr + '!' + "<br>");
+	}
+}
+});
