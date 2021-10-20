0701c61c19e8651c2c2042756efdcc6847369059
holyneotrin <tphanlow@csumb.edu>
2021-10-19 21:24:02 -0700

    lab 14 update

--- lab14/js/lab14.js
+++ lab14/js/lab14.js
@@ -1,3 +1,23 @@
 // author: trinity phan-low
 // date: 10.18.2021
 // public domain
+
+for (num=1; num <=200; num++) {
+		if (num % 15 == 0){
+    	console.log("FizzBuzz!")
+    }
+    //multiples of 3
+    if (num % 3 == 0) {
+        console.log("Fizz!");
+    }
+    // multiples of 5
+    if (num % 5 == 0) {
+      console.log("Buzz!");
+    }
+    if (num % 7 == 0){
+      console.log("Boom!");
+    }
+    else {
+        console.log(num);
+    }
+    
